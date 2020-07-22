43cbe674f43053b334c08e81451767753fcc491d
Chow Jia Ying <chowjiaying211@gmail.com>
2020-07-23 01:22:25 +0800

    Incorporate NavBar into App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import {Layout} from 'antd';
 import logo from './logo.svg';
 import './App.css';
+
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <NavBar />
+      </Layout>
     </div>
   );
 }
