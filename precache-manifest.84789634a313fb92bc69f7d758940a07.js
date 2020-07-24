0cf70107e7001fa0acb0fb540f2c76d5ab141c9c
Chow Jia Ying <chowjiaying211@gmail.com>
2020-07-24 10:46:26 +0800

    Updates

---
+++ precache-manifest.84789634a313fb92bc69f7d758940a07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76c87b56c788d0b205cc65e4f4750b7",
+    "url": "/ant-responsive-sidebar/index.html"
+  },
+  {
+    "revision": "1cccea5420416c3d7f5d",
+    "url": "/ant-responsive-sidebar/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "b29077ac023f8ce098e3",
+    "url": "/ant-responsive-sidebar/static/css/main.f126eb83.chunk.css"
+  },
+  {
+    "revision": "1cccea5420416c3d7f5d",
+    "url": "/ant-responsive-sidebar/static/js/2.9cc8062f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ant-responsive-sidebar/static/js/2.9cc8062f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29077ac023f8ce098e3",
+    "url": "/ant-responsive-sidebar/static/js/main.041422d7.chunk.js"
+  },
+  {
+    "revision": "b0afd29459bc0a23c7c0",
+    "url": "/ant-responsive-sidebar/static/js/runtime-main.d715683a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ant-responsive-sidebar/static/media/logo.5d5d9eef.svg"
+  }
+]);
