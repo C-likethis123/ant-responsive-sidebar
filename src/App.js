47baa37c3d3e8de30a468983000385eadc2efb61
Chow Jia Ying <chowjiaying211@gmail.com>
2020-07-25 01:11:59 +0800

    Format code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, {useState} from 'react';
-import { Layout } from 'antd';
-import TopicMenu from "./components/TopicMenu"
+import React, { useState } from "react";
+import { Layout } from "antd";
+import TopicMenu from "./components/TopicMenu";
 
-import './App.css';
+import "./App.css";
 
 import NavBar from "./components/NavBar/NavBar";
-import SideBar from './components/SideBar/SideBar';
+import SideBar from "./components/SideBar/SideBar";
 
 function App() {
-  const topics = ['First topic', 'Second topic', 'Third topic']
-  const [contentIndex, setContentIndex] = useState(0)
+  const topics = ["First topic", "Second topic", "Third topic"];
+  const [contentIndex, setContentIndex] = useState(0);
   const [selectedKey, setSelectedKey] = useState("0");
   const changeSelectedKey = (event) => {
-    const key = event.key
-    setSelectedKey(key)
-    setContentIndex(+key)
-  }
-  const Menu = <TopicMenu topics={topics} selectedKey={selectedKey} changeSelectedKey={changeSelectedKey}/>
+    const key = event.key;
+    setSelectedKey(key);
+    setContentIndex(+key);
+  };
+  const Menu = (
+    <TopicMenu
+      topics={topics}
+      selectedKey={selectedKey}
+      changeSelectedKey={changeSelectedKey}
+    />
+  );
   return (
     <div className="App">
-      <NavBar menu={Menu}/>
+      <NavBar menu={Menu} />
       <Layout>
         <SideBar menu={Menu} />
         <Layout.Content className="content">
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,30 +1,31 @@
-import React, {useState} from "react";
-import {Drawer, Button} from "antd";
-import {MenuOutlined} from "@ant-design/icons";
+import React, { useState } from "react";
+import { Drawer, Button } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
 import "./NavBar.css";
-import logo from './../../logo.svg';
+import logo from "./../../logo.svg";
 
-const NavBar = ({menu}) => {
+const NavBar = ({ menu }) => {
   const [visible, setVisible] = useState(false);
   return (
     <nav className="navbar">
       <Button
-          className="menu"
-          type="primary"
-          icon={<MenuOutlined />}
-          onClick={() => setVisible(true)}
+        className="menu"
+        type="primary"
+        icon={<MenuOutlined />}
+        onClick={() => setVisible(true)}
       />
       <Drawer
-            title="Topics"
-            placement="left"
-            onClick={() => setVisible(false)}
-            onClose={() => setVisible(false)}
-            visible={visible}
-          >
-          {menu}
+        title="Topics"
+        placement="left"
+        onClick={() => setVisible(false)}
+        onClose={() => setVisible(false)}
+        visible={visible}
+      >
+        {menu}
       </Drawer>
-      <a href="/"><img src={logo} className="logo" alt="logo" /></a>
-      
+      <a href="/">
+        <img src={logo} className="logo" alt="logo" />
+      </a>
     </nav>
   );
 };
